e14cb8dcc6e91dfa6650d9b1fc91d05546e1ee8b
morgannoon <112260476+morgannoon@users.noreply.github.com>
2022-09-19 15:29:01 -0400

    Update decomposingMoney.java

--- Lab2/men83_Assignment1/src/men83_Assignment1/decomposingMoney.java
+++ Lab2/men83_Assignment1/src/men83_Assignment1/decomposingMoney.java
@@ -1,10 +1,21 @@
 package men83_Assignment1;
-
+import javax.swing.JOptionPane;
 public class decomposingMoney {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	//input
+		String input = JOptionPane.showInputDialog("Enter an amount of money between $1-$9999");
+		int number = (int)Double.parseDouble(input);
+	//bucks
+		int bucks = number%10;
+	//sawbucks
+		int sawbucks = (number%100)/10;
+	//benjamins
+		int benjamins = (number%1000)/100;
+	//grands
+		int grands = number/1000;
+	//output
+		System.out.println(grands+" grands\n" +benjamins+" benjamins\n"+ sawbucks+ " sawbucks\n"+ bucks+ " bucks");
 	}
 
 }
