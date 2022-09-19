1b35b5af937eaf6392eb801dc23b894c4d542876
morgannoon <112260476+morgannoon@users.noreply.github.com>
2022-09-19 15:28:41 -0400

    Update PythagoreanTheorem.java

--- Lab2/men83_Assignment1/src/men83_Assignment1/PythagoreanTheorem.java
+++ Lab2/men83_Assignment1/src/men83_Assignment1/PythagoreanTheorem.java
@@ -1,10 +1,18 @@
 package men83_Assignment1;
-
+import javax.swing.JOptionPane;
 public class PythagoreanTheorem {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	//inputed #'s for triangle
+		String numberInput1 =JOptionPane.showInputDialog("Input a number");
+		Double number1=Double.parseDouble(numberInput1);
+		String numberInput2= JOptionPane.showInputDialog("Input another number");
+		Double number2 = Double.parseDouble(numberInput2);
+	//hypotenuse
+		double hyp = Math.sqrt((Math.pow(number1, 2))+(Math.pow(number2, 2)));
+		hyp=(int)Math.round(hyp*100);
+		hyp=hyp/100.0;
+		System.out.println("The hypotenuse is "+ hyp +".");
 	}
 
 }
