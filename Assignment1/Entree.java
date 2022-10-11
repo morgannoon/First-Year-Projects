1f485b4e3040f380c2f31cd0c6abb5d9cb4b6488
morgannoon <112260476+morgannoon@users.noreply.github.com>
2022-10-11 10:43:32 -0400

    Add files via upload

---
+++ Assignment1/Entree.java
@@ -0,0 +1,35 @@
+package men83_MenuManager;
+/**
+ * Class Entree
+ * author: Morgan Noonan
+ *created: 10/3/2022
+ */
+public class Entree{
+	private String name;
+	private String desc;
+	private int cal;
+	public Entree(String n, String d, int c) {
+		name=n;
+		desc=d;
+		cal=c;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	public int getCal() {
+		return cal;
+	}
+	public void setCal(int cal) {
+		this.cal = cal;
+	}
+}
